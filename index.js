2a1737e540060dfdf8184a8234ab648ecd2f707e
Kazuyuki Iijima <iijimakazuyuki@gmail.com>
2019-04-29 10:08:31 +0900

    Add main function

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const BillingReporter = require('./billing_reporter');
 const PROJECT_ID = process.env.PROJECT_ID;
 const BILLING_ACCOUNT_ID = process.env.BILLING_ACCOUNT_ID.replace(/-/g, '_');
 
-const billingReporter = new BillingReporter(
-    PROJECT_ID,
-    BILLING_ACCOUNT_ID,
-);
 
-billingReporter.query();
+function main() {
+    const billingReporter = new BillingReporter(
+        PROJECT_ID,
+        BILLING_ACCOUNT_ID,
+    );
+
+    billingReporter.query();
+}
